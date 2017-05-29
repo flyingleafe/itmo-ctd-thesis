64750dcb68361ff934cd73092020d843a47b94fd
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2017-05-30 01:05:20 +0300

    update

---
+++ default.nix
@@ -0,0 +1,34 @@
+with import <nixpkgs> {}; {
+  thesisTextEnv = stdenv.mkDerivation {
+    name = "thesis-text-env";
+    buildInputs = [
+      (texlive.combine {
+        inherit (texlive) 
+          scheme-small
+          biber
+          collection-langcyrillic
+          csquotes
+          tabu
+          varwidth
+          floatrow
+          algorithms
+          algorithmicx
+          enumitem
+          biblatex
+          biblatex-gost
+          logreq
+          xstring
+          lastpage
+          totcount
+          chngcntr
+          titlesec
+          paratype
+          was
+          filecontents
+	  cm-super;
+      })
+      biber
+      graphviz
+    ];
+  };
+}
